00cf9f007d46554b35c1d47f0fac90f1ccf3a9c1
shahoo07 <shahoob2020@gmail.com>
2026-02-08 00:24:39 +0530

    comminting the user models

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env",
+});
+
+const app = express();
+const port =  process.env.PORT || 3000;
+
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended:true,limit: "16kb"}))
+app.use(express.static("public"))
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials:true,
+    methods: ["GET","PUT","DELETE","POST","PATCH","OPTIONS"],
+    allowedHeaders: ["Content-Type","Authorization"],
+}))
+
+import healthCheckRouter from "./routes/health.routes.js";
+
+app.use("/api/v1/healthcheck",healthCheckRouter);
+
+app.get('/',(req,res) =>{
+    res.send("hello world");
+});
+
+connectDB()
+    .then(() =>{
+        app.listen( port ,()=>{
+    console.log("server is running");
+})
+    })
+    .catch((err) =>{
+        console.error("MongoDB connection error",err)
+        process.exit(1)
+    })
